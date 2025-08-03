e54ddbc984142393b88dbfaefc9b3cde723151e4
No <AyEmREL@protonmail.com>
2025-08-03 19:16:46 +0300

    Work on text selector fragment

--- app/src/main/java/com/example/hellenicreaderapp/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/hellenicreaderapp/ui/dashboard/DashboardViewModel.kt
@@ -3,11 +3,17 @@ package com.example.hellenicreaderapp.ui.dashboard
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.hellenicreaderapp.AppState
 
 class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Here you can choose texts to read"
+    // Change this to be the title of the last text read instead of the file name
+    private val _lastReadText = MutableLiveData<String>().apply {
+        value = AppState.lastRead
     }
-    val text: LiveData<String> = _text
+    val lastReadText: LiveData<String> = _lastReadText
+
+    fun refreshLastRead() {
+        _lastReadText.value = AppState.lastRead
+    }
 }
