845d718a93e8b7b279a659d816e0c500aad22ebb
Blagai <relarealm@gmail.com>
2026-03-16 21:53:04 +0200

    Reader next button
    Also added Greek texts for some more hymns and some layout for a save button

--- app/src/main/java/com/example/hellenicreaderapp/AppState.kt
+++ app/src/main/java/com/example/hellenicreaderapp/AppState.kt
@@ -8,4 +8,18 @@ object AppState {
     var lastRead: String? = null
 
     var readTextsNum: Int? = 0
+
+    var basicHymnReadingOrder = listOf("hohy1", "hohy2", "hohy3", "hohy4", "hohy5", "hohy6")
+
+    var readingOrder = orderOfReading.BASICHYMNS // Hardcoded test case
+
+    enum class orderOfReading {
+        BASICHYMNS
+    }
+
+    fun getCurrentOrder(): List<String> {
+        return when (readingOrder) {
+            orderOfReading.BASICHYMNS -> basicHymnReadingOrder
+        }
+    }
 }
