1fc46cc81d73a1087c66637fe2e35ae3a5e90a1d
Blagai <relarealm@gmail.com>
2026-03-16 21:53:05 +0200

    Update DataParser.kt

--- app/src/main/java/com/example/hellenicreaderapp/utility/DataParser.kt
+++ app/src/main/java/com/example/hellenicreaderapp/utility/DataParser.kt
@@ -3,13 +3,14 @@ package com.example.hellenicreaderapp.utility
 import java.io.InputStream
 
 object DataParser {
-    var litTranslation: String = """"""
+
     var litTranslations: List<String> = emptyList()
+    fun loadLitTranslations(inputStream: InputStream) {
+        litTranslations = parseCsvToLines(inputStream)
+    }
 
     fun parseCsvToLines(inputStream: InputStream): List<String> {
         val lines = inputStream.bufferedReader().use { it.readLines() }.filter { it.isNotBlank() }
-        litTranslations = lines
-        litTranslation = lines.joinToString("\n")
         return lines
     }
 }
