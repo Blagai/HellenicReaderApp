098a0e7ea01a2f78928e187596b081ab38f2068c
Blagai <relarealm@gmail.com>
2026-03-22 19:11:46 +0200

    Fix Gradle build issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -15,15 +17,7 @@ kotlin {
     compilerOptions {
         languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
     }
-    sourceSets {
-        androidMain {
-
-        }
+    project.extensions.findByType<KotlinMultiplatformExtension>()?.let { kmpExt ->
+        kmpExt.sourceSets.removeAll {it.name == "commonTest"}
     }
 }
-
-
-
-
-
-
