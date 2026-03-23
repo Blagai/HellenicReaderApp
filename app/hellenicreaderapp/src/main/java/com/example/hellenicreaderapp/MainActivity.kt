17ee8f7692f4c91e72c5e6530316835dde1700d5
Blagai <relarealm@gmail.com>
2026-03-23 20:07:51 +0200

    Delete unused imports

--- app/hellenicreaderapp/src/main/java/com/example/hellenicreaderapp/MainActivity.kt
+++ app/hellenicreaderapp/src/main/java/com/example/hellenicreaderapp/MainActivity.kt
@@ -3,15 +3,14 @@ package com.example.hellenicreaderapp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.hellenicreaderapp.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
 import com.example.hellenicreaderapp.utility.DataParser
 import com.example.hellenicreaderapp.utility.dataStoreManager
+import com.google.android.material.tabs.TabLayout
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
