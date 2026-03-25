ab89a81867461f72b041ee819cb91db016979c71
Blagai <relarealm@gmail.com>
2026-03-25 20:48:18 +0200

    Better about page

--- app/hellenicreaderapp/src/main/java/com/blagai/hellenicreaderapp/ui/misc/AboutFragment.kt
+++ app/hellenicreaderapp/src/main/java/com/blagai/hellenicreaderapp/ui/misc/AboutFragment.kt
@@ -1,20 +1,18 @@
 package com.blagai.hellenicreaderapp.ui.misc
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.blagai.hellenicreaderapp.R
 import com.google.android.material.tabs.TabLayout
 
-
 class AboutFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        (activity as AppCompatActivity).supportActionBar!!.hide()
         (activity?.findViewById<TabLayout>(R.id.top_tab_layout))?.visibility = View.GONE
     }
 
@@ -22,14 +20,7 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_about, container, false)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-
-        (activity as AppCompatActivity).supportActionBar!!.show()
-        (activity?.findViewById<TabLayout>(R.id.top_tab_layout))?.visibility = View.VISIBLE
     }
 }
